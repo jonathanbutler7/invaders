9a5a13f43e049ca0b86bbc16a50747e692ba6342
Jonathan Butler <jonathan.butler@getweave.com>
2023-08-20 16:41:36 -0500

    add player

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use crossterm::{
     ExecutableCommand,
 };
 use invaders::{
-    frame::{self, new_frame},
+    frame::{self, new_frame, Drawable},
+    player::Player,
     render,
 };
 use rusty_audio::Audio;
@@ -49,14 +50,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
+    let mut player = Player::new();
+
     // Game Loop
     'gameloop: loop {
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -66,6 +71,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
         // draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
